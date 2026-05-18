8ae778c77197bb58c2fd733c556e192011a1f53c
piyushchoudhary2005 <piyush2005.ise@gmail.com>
2026-05-18 09:17:45 +0530

    jenkincheck1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
       <h1>Frontend Running</h1>
-      <h1>Hello Jenkins CI/CD 🚀</h1>
+      <h1>Hello Jenkins CI/CD1 🚀</h1>
       <h2>{message}</h2>
     </div>
   );
